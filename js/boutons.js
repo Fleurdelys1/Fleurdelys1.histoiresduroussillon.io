10739dde80270ff60c79cff05c2097f1495ac92f
Fleurdelys1 <98351679+Fleurdelys1@users.noreply.github.com>
2023-03-28 01:48:50 -0400

    index

---
+++ js/boutons.js
@@ -0,0 +1,32 @@
+let couleurOriginale;
+
+window.addEventListener("load", () => {
+    let Buttons = document.querySelectorAll("#bouton");
+    let header = document.querySelectorAll(".header-container");
+    
+    for (let i = 0; i < Buttons.length; i++){
+        Buttons[i].addEventListener("mouseenter", () => {
+            couleurOriginale = Buttons[i].style.backgroundColor;
+            fadeIn(Buttons[i], Buttons[i].style.opacity);
+        })
+        Buttons[i].addEventListener("mouseleave", () => {
+            fadeOut(Buttons[i], Buttons[i].style.opacity);
+        })
+    }
+    console.log(header)
+    header[0].addEventListener("click", () => {window.scrollTo(0,0)});
+    header[1].addEventListener("click", () => {window.open("../html/Categories.html")})
+    
+})
+
+const fadeIn = (element, opacity) => {
+    element.style.backgroundColor= "rgba(255,255,255,0.3)"
+}
+
+const fadeOut = (element, opacity) => {
+    element.style.backgroundColor= couleurOriginale;
+}
+
+const openArticle = (num) =>{
+    window.open("../html/art_"+num+".html",'_blank');
+}
